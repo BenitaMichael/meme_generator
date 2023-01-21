e6e359fed00f60087113ed9a4d36d00f939c1743
BenitaMichael <107327075+BenitaMichael@users.noreply.github.com>
2023-01-21 14:04:08 +0100

    completed work

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return(
+        <div className="nav-bar">
+            <div className="nav">
+                <img src="./images/logo.jpg" alt="logo" className="logo"/>
+                <h2>Meme Generator</h2>
+            </div>
+            <h4>React Course - Project 3</h4>
+        </div>
+    )
+}
